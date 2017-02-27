726022b6b18def5109015f113f3708101a7863a2
Antonio Esposito <antonioesposito@mbp-di-antonio.lan>
2017-02-27 14:19:29 +0100

    Azione dev 1

--- repo2test/ViewController.swift
+++ repo2test/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func azione1click(_ sender: Any) {
+        print("azione dev 1")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
